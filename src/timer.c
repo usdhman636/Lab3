992a33e9cca6295efac0888d89335d59317b94b3
usdhman636 <adamayyash20072@gmail.com>
2025-12-19 13:40:08 +0300

    removed arrays from timer

--- src/timer.c
+++ src/timer.c
@@ -1,22 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
 #include "timer.h"
 #include "sort.h"
-#include <time.h>
-#include <string.h>
 
-double measure_insertion(int arr[], int n) {
-    int copy[n];
-    memcpy(copy, arr, sizeof(int) * n);
+double measure_insertion(const int *arr, int n) {
+    int *tmp = malloc(n * sizeof(int));
+    if (!tmp) {
+        return -1.0;
+    }
+
+    memcpy(tmp, arr, n * sizeof(int));
 
     clock_t start = clock();
-    insertion_sort(copy, n);
-    return (double)(clock() - start) / CLOCKS_PER_SEC;
+    insertion_sort(tmp, n);
+    clock_t end = clock();
+
+    free(tmp);
+
+    return (double)(end - start) / CLOCKS_PER_SEC;
 }
 
-double measure_merge(int arr[], int n) {
-    int copy[n];
-    memcpy(copy, arr, sizeof(int) * n);
+double measure_merge(const int *arr, int n) {
+    int *tmp = malloc(n * sizeof(int));
+    if (!tmp) {
+        return -1.0;
+    }
+
+    memcpy(tmp, arr, n * sizeof(int));
 
     clock_t start = clock();
-    merge_sort(copy, 0, n - 1);
-    return (double)(clock() - start) / CLOCKS_PER_SEC;
+    merge_sort(tmp, 0, n - 1);
+    clock_t end = clock();
+
+    free(tmp);
+
+    return (double)(end - start) / CLOCKS_PER_SEC;
 }
