7f4595e2539cc5a47220a3edc2ce62d456992400
usdhman636 <adamayyash20072@gmail.com>
2025-12-19 14:44:31 +0300

    removed arrays from stach files

--- src/sort.c
+++ src/sort.c
@@ -1,47 +1,68 @@
-#include "sort.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "sort.h"
 
-void insertion_sort(int arr[], int n) {
+// -------------------- Insertion Sort --------------------
+void insertion_sort(int *arr, int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i];
+        int key = *(arr + i);
         int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        while (j >= 0 && *(arr + j) > key) {
+            *(arr + j + 1) = *(arr + j);
             j--;
         }
-        arr[j + 1] = key;
+        *(arr + j + 1) = key;
     }
 }
 
-static void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
+// -------------------- Merge Function --------------------
+void merge(int *arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
+    // Allocate temporary arrays dynamically
     int *L = malloc(n1 * sizeof(int));
     int *R = malloc(n2 * sizeof(int));
 
     for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int i = 0; i < n2; i++)
-        R[i] = arr[m + 1 + i];
+        *(L + i) = *(arr + left + i);
+    for (int j = 0; j < n2; j++)
+        *(R + j) = *(arr + mid + 1 + j);
 
-    int i = 0, j = 0, k = l;
+    int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2) {
-        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+        if (*(L + i) <= *(R + j)) {
+            *(arr + k) = *(L + i);
+            i++;
+        } else {
+            *(arr + k) = *(R + j);
+            j++;
+        }
+        k++;
     }
 
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+    while (i < n1) {
+        *(arr + k) = *(L + i);
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        *(arr + k) = *(R + j);
+        j++;
+        k++;
+    }
 
     free(L);
     free(R);
 }
 
-void merge_sort(int arr[], int left, int right) {
+// -------------------- Merge Sort --------------------
+void merge_sort(int *arr, int left, int right) {
     if (left < right) {
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         merge_sort(arr, left, mid);
         merge_sort(arr, mid + 1, right);
         merge(arr, left, mid, right);
--- src/sort.h
+++ src/sort.h
@@ -1,7 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
-void insertion_sort(int arr[], int n);
-void merge_sort(int arr[], int left, int right);
+
+void insertion_sort(int *arr, int n);
+
+void merge(int *arr, int left, int mid, int right);
+
+void merge_sort(int *arr, int left, int right);
 
 #endif
